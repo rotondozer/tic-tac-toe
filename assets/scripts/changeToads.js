3306cea4db8dc9aa2543be117c6c7b5cc8177ebc
rotondozer <nickrotondo724@gmail.com>
2017-07-31 21:18:04 -0400

    Assigns changeToads function and exports

--- assets/scripts/changeToads.js
+++ assets/scripts/changeToads.js
@@ -4,9 +4,11 @@ const toads = require('./toads')
 
 // trying to eliminate loop since array.length is only 2?
 //  if not one, must be the other
-for (let i = 0; i < toads.length; i++) {
-  toads[i].active = !toads[i].active
+const changeToads = function () {
+  for (let i = 0; i < toads.length; i++) {
+    toads[i].active = !toads[i].active
+  }
 }
 console.log(toads)
 
-// module.exports =
+module.exports = changeToads
