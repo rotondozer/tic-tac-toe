dab4dc2de1af510b4ac74d3ad09454a8694c6d5d
rotondozer <nickrotondo724@gmail.com>
2017-08-02 13:04:24 -0400

    Removes console.log statements and 'winner' alert

--- assets/scripts/winLogic.js
+++ assets/scripts/winLogic.js
@@ -12,14 +12,10 @@ const winConditions = [
 ]
 
 const compareArrays = function (array, array2d) {
-  console.log('i\'ve been called on ' + array + ' and ' + array2d)
-  console.log(array2d[1])
   for (let i = 0; i < array.length; i++) {
     if (array[i] === array2d[1]) {
-      console.log('made it')
       for (let j = 0; j < array.length; j++) {
         if (array[j] === array2d[2]) {
-          console.log('WINNER!')
           alert('WINNER')
           return true
         }
@@ -29,14 +25,10 @@ const compareArrays = function (array, array2d) {
 }
 
 const checkWin = function (toadParam, callback) {
-  // move sort() out after debugging?
   toadParam.sort()
-  console.log(toadParam)
   for (let i = 0; i < toadParam.length; i++) {
     for (let j = 0; j < winConditions.length; j++) {
       if (toadParam[i] === winConditions[j][0]) {
-        console.log(toadParam[i])
-        console.log(winConditions[j])
         callback(toadParam, winConditions[j])
       }
     }
