c607b770575ea06c9161be6b0af97418b8bdc619
rotondozer <nickrotondo724@gmail.com>
2017-08-03 15:32:53 -0400

    Updates input fields, adds IDs to buttons

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const authEvents = require('./auth/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -10,9 +11,6 @@ $(() => {
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-const authEvents = require('./auth/events.js')
-
-// On document ready
 
 // use require without a reference to ensure a file is bundled
 require('./example')
